a46d6c5a1e1c4d18d914f35fa2d7acb09a3087b7
RIP21 <puha212@gmail.com>
2020-09-06 19:07:04 +0200

    Initial commit

---
+++ src/array/index.ts
@@ -0,0 +1,7 @@
+export { useBindToInput, UseBindToInput, BindToInput } from './useBindToInput';
+export { useArray, UseArray, UseArrayActions } from './useArray';
+export { useBoolean, UseBoolean, UseBooleanActions } from './useBoolean';
+export { useInput, UseInput, UseInputActions } from './useInput';
+export { useMap, UseMap, MapOrEntries, UseMapActions, UseMapFunctions } from './useMap';
+export { useNumber, UseNumber, UseNumberActions } from './useNumber';
+export { useSetState, UseSetState, UseSetStateAction } from './useSetState';
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { useArray, UseArray } from './useArray';
+export { useBoolean, UseBoolean } from './useBoolean';
+export { useInput, UseInput } from './useInput';
+export { useLogger } from './useLogger';
+export { useMap, UseMap, MapOrEntries } from './useMap';
+export { useNumber, UseNumber } from './useNumber';
+export { usePrevious } from './usePrevious';
+export { useSetState, UseSetState, UseSetStateAction } from './useSetState';
+export { useStateful, UseStateful } from './useStateful';
